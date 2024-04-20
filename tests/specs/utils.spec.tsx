5dae8b0ea7966499dffde8901a10e7116fe68630
Cengiz Ulusoy <Cengizism@users.noreply.github.com>
2024-04-20 18:03:26 +0200

    test(utils): Added unit tests for the utils module

---
+++ tests/specs/utils.spec.tsx
@@ -0,0 +1,74 @@
+import {
+  createSlug,
+  degreesToRadians,
+  randomizeCompetencies,
+} from "../../src/utils";
+import slugify from "slugify";
+import { TemplateWithRandomCompetenciesType } from "../../typings";
+
+jest.mock("slugify");
+
+describe("Utils", () => {
+  describe("createSlug", () => {
+    it("should create a slug with a timestamp and the input string", () => {
+      (slugify as unknown as jest.Mock).mockReturnValue("test-string");
+      const slug = createSlug("Test String");
+      expect(slug).toMatch(/^\d+-test-string$/);
+    });
+  });
+
+  describe("degreesToRadians", () => {
+    it("should convert degrees to radians", () => {
+      const radians = degreesToRadians(180);
+      expect(radians).toBe(Math.PI);
+    });
+  });
+
+  describe("randomizeCompetencies", () => {
+    it("should randomize competencies and select three", () => {
+      const templates: TemplateWithRandomCompetenciesType[] = [
+        {
+          title: "Template 1",
+          slug: { _type: "slug", current: "template-1" },
+          competencies: [
+            {
+              title: "Competency 1",
+              description: "",
+              value: 0,
+              improvement: false,
+            },
+            {
+              title: "Competency 2",
+              description: "",
+              value: 0,
+              improvement: false,
+            },
+            {
+              title: "Competency 3",
+              description: "",
+              value: 0,
+              improvement: false,
+            },
+            {
+              title: "Competency 4",
+              description: "",
+              value: 0,
+              improvement: false,
+            },
+            {
+              title: "Competency 5",
+              description: "",
+              value: 0,
+              improvement: false,
+            },
+          ],
+          randomCompetencies: "",
+        },
+      ];
+      const randomizedTemplates = randomizeCompetencies(templates);
+      expect(randomizedTemplates[0].randomCompetencies.split(", ").length).toBe(
+        3
+      );
+    });
+  });
+});
