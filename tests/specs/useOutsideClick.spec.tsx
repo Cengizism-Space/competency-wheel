6f7360b9bc4f0a95d23d52ed38cd951536312d39
Cengiz Ulusoy <Cengizism@users.noreply.github.com>
2024-04-21 10:41:07 +0200

    test(useOutsideClick): Added unit tests

---
+++ tests/specs/useOutsideClick.spec.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { render, fireEvent } from "@testing-library/react";
+import { useRef } from "react";
+import useOutsideClick from "../../src/hooks/useOutsideClick";
+
+describe("useOutsideClick", () => {
+  it("calls the callback when clicked outside", () => {
+    const callback = jest.fn();
+
+    function TestComponent() {
+      const ref = useRef(null);
+      useOutsideClick(ref, callback);
+
+      return <div ref={ref}>Test</div>;
+    }
+
+    const { container } = render(<TestComponent />);
+    fireEvent.mouseDown(container);
+
+    expect(callback).toHaveBeenCalled();
+  });
+
+  it("does not call the callback when clicked inside", () => {
+    const callback = jest.fn();
+
+    function TestComponent() {
+      const ref = useRef(null);
+      useOutsideClick(ref, callback);
+
+      return <div ref={ref}>Test</div>;
+    }
+
+    const { getByText } = render(<TestComponent />);
+    fireEvent.mouseDown(getByText("Test"));
+
+    expect(callback).not.toHaveBeenCalled();
+  });
+});
