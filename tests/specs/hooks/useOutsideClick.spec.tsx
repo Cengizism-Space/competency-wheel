fa1067c4163232dc5f8b327efca4642fc50594a4
Cengiz Ulusoy <Cengizism@users.noreply.github.com>
2024-04-21 17:47:38 +0200

    test(useOutsideClick): Add more tests

--- tests/specs/hooks/useOutsideClick.spec.tsx
+++ tests/specs/hooks/useOutsideClick.spec.tsx
@@ -20,6 +20,32 @@ describe("useOutsideClick", () => {
     expect(callback).toHaveBeenCalled();
   });
 
+  it("does not call the callback when clicked on .competency-value-controllers, .competency-remove, or form", () => {
+    const callback = jest.fn();
+  
+    function TestComponent() {
+      const ref = useRef(null);
+      useOutsideClick(ref, callback);
+  
+      return (
+        <div>
+          <div ref={ref}>
+            <div className="competency-value-controllers">Test 1</div>
+            <div className="competency-remove">Test 2</div>
+            <form>Test 3</form>
+          </div>
+        </div>
+      );
+    }
+  
+    const { getByText } = render(<TestComponent />);
+    fireEvent.mouseDown(getByText("Test 1"));
+    fireEvent.mouseDown(getByText("Test 2"));
+    fireEvent.mouseDown(getByText("Test 3"));
+  
+    expect(callback).not.toHaveBeenCalled();
+  });
+
   it("does not call the callback when clicked inside", () => {
     const callback = jest.fn();
 
